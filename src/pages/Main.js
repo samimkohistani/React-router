5433ee31efb7f52610bd070caf9f83d9a4701bce
Samim Kohistani <samimkohistani@Samims-MacBook-Air.local>
2022-09-26 02:37:18 -0400

    stock-main

---
+++ src/pages/Main.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Dashboard from './Dashboard';
+import About from './About';
+import Stock from './Stock';
+import stockData from '../data.json'
+
+function Main(props) {
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route
+          path="/stocks/:symbol"
+          render={(props) => < Stock stockData={stockData} {...props} />
+          }
+        />
+        <Route
+          path="/stocks"
+          render={(props) => <Dashboard {...props} stockData={stockData} />
+          }
+        />
+      </Switch>
+    </main>
+  );
+}
+
+export default Main;
